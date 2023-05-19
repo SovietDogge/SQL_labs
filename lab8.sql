fc95f9a50ef4c8ea91c0e838d4713dbbf9bc16f0
Soviet.Dog <soviet.dog1945@proton.me>
2023-05-19 09:44:23 +0300

    fix lab8

--- lab8.sql
+++ lab8.sql
@@ -5,27 +5,26 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `authors`;
-CREATE TABLE `authors` (
-  `project_id` tinyint(3) unsigned NOT NULL,
-  `author_id` tinyint(4) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `file_id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`author_id`),
-  KEY `project_id` (`project_id`),
-  CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+  PRIMARY KEY (`file_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `files`;
-CREATE TABLE `files` (
+DROP TABLE IF EXISTS `files_projects`;
+CREATE TABLE `files_projects` (
+  `id` tinyint(255) unsigned NOT NULL AUTO_INCREMENT,
+  `file_id` tinyint(4) NOT NULL,
   `project_id` tinyint(3) unsigned NOT NULL,
-  `task_id` tinyint(3) unsigned NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `file_id` (`file_id`),
   KEY `project_id` (`project_id`),
-  KEY `task_id` (`task_id`),
-  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
-  CONSTRAINT `files_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `files_projects_ibfk_1` FOREIGN KEY (`id`) REFERENCES `branches` (`id`),
+  CONSTRAINT `files_projects_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`),
+  CONSTRAINT `files_projects_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `projects`;
@@ -40,6 +39,7 @@ DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
   `task_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` tinyint(3) unsigned NOT NULL,
+  `author_id` tinyint(3) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`task_id`),
@@ -48,16 +48,51 @@ CREATE TABLE `tasks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `project_id` tinyint(3) unsigned NOT NULL,
-  `user_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `project_id` (`project_id`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
-  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `projects` (`project_id`)
+DROP TABLE IF EXISTS `tasks_files`;
+CREATE TABLE `tasks_files` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `task_id` tinyint(3) unsigned NOT NULL,
+  `file_id` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_id` (`task_id`),
+  KEY `file_id` (`file_id`),
+  CONSTRAINT `tasks_files_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`),
+  CONSTRAINT `tasks_files_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2023-05-12 07:14:35
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `users_project`;
+CREATE TABLE `users_project` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `user_id` tinyint(3) unsigned NOT NULL,
+  `project_id` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `users_project_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `users_project_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `users_tasks`;
+CREATE TABLE `users_tasks` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `user_id` tinyint(3) unsigned NOT NULL,
+  `task_id` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `users_tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `users_tasks_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2023-05-19 06:43:04
